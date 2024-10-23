45ea5b5bc3d229e5a17c74ce31de4092450dc89e
kowalskiw <kowalskiwojciech986@gmail.com>
2024-10-23 12:46:04 +0200

    webgui added

--- webgui/index.php
+++ webgui/index.php
@@ -1,41 +1,61 @@
 <?php
+
+echo "<link rel='stylesheet' type='text/css'  href='style.css'>";
+
+function showHeader($header){
+    echo "<div class='header'>";
+        echo "<a href='index.php'><img src='img/home.png' alt='Home' class='header-picture'></a>";
+        echo "<a href='https://aamks.apoz.edu.pl/'><img src='img/tools.png' alt='Tools' class='header-picture'></a>";
+        echo "<div class='header-text'>";
+            echo "$header";
+        echo "</div>";
+    echo "</div>";
+}
+function showFooter(){
+    echo "<div class='footer'>";
+        echo "Instytut Inżynierii Bezpieczeństwa, Akademia Pożarnicza w Warszawie<br>";
+        echo "<a href=mailto:wkowalski@apoz.edu.pl>wkowalski@apoz.edu.pl</a>";
+    echo "</div>";
+}
+function showForm($head, $form){
+    $form .= "";
+    showHeader($head);
+    echo "<div class='content'>
+        $form
+        </div>";
+    showFooter();
+}
 function showFilesSubmissionForm(){
     $lang = "pl";
-    $head = array(
-        "pl" => "<h1>Przesyłanie Plików</h1>",
-        "en" => "<h1>Files Submission</h1>",
-    );
-    $send = array(
-        "pl" => "Wyślij",
-        "en" => "Send",
-    );
+    $head = "Przesyłanie Plików";
+    $send = "Wyślij";
 
-    $form = "<form method='POST' enctype='multipart/form-data' action='?stage=send_files'>
+    $form = "
+
+    <form method='POST' enctype='multipart/form-data' action='?stage=send_files'>
+    <p>Wgraj pliki niezbędne do obliczenia wymaganego zasięgu widzialności.</p>
+    <br><br>
         <table>
             <tr><td><i>*_devc.csv</i>   </td>
               <td><input type='file' name='devc_file' id='devc_file' accept='.csv' required></td></tr>
             <tr><td><i>*.fds</i>
               <td><input type='file' name='fds_file' id='fds_file' accept='.fds,.txt' required></td></tr>
             </table>
-        <input type='submit' name='send_files' value='$send[$lang]'>
+            <br><br>
+        <input type='submit' name='send_files' value='$send'>
         </form>";
-    echo $head[$lang];
-    echo $form;
+    showForm($head, $form);
 }
 
 function showBeamSelectionForm(){
     $lang = "pl";
-    $head = array(
-        "pl" => "<h1>Wybranie BEAM</h1>",
-        "en" => "<h1>BEAM Selection</h1>",
-    );
-    $send = array(
-        "pl" => "Wyślij",
-        "en" => "Send",
-    );
+    $head = "Wybór BEAM";
+    $send = "Wyślij";
+
     $form = "
         <form method='POST' action='?stage=select_beams'>
-            <p>Zaznacz lub odznacz, aby wybrać BEAM.</p>
+            <p>Zaznacz lub odznacz, aby wybrać BEAM</p>
+            <br><br>
             ";
     foreach ($_SESSION['beams'] as $beam){
         $spec_beam = htmlspecialchars($beam);
@@ -46,13 +66,12 @@ function showBeamSelectionForm(){
         }
     $form .= "
             <input type='hidden' name='beam_order' id='beam_order'>
-            <br>
+            <br><br>
             <button type='submit'>Przetwórz wybrane BEAM</button>
         </form>
     ";
 
-    echo $head[$lang];
-    echo $form;
+    showForm($head, $form);
 }
 
 function showAlarmTimeForm(){
@@ -63,35 +82,40 @@ function showAlarmTimeForm(){
     $alarm_time = $_SESSION['alarm_time'];
 
 
-    $head = array(
-        "pl" => "<h1>Potwierdź czas alarmowania</h1>",
-        "en" => "<h1>Confirm alarm time</h1>",
-    );
+    $head = "Czas alarmowania";
 
     $form = "
     <form action='?stage=confirm_alarm' method='POST'>
-        <label for='alarm_time'>Czas Alarmu:</label>
-        <input type='text' name='alarm_time' id='alarm_time' value='$alarm_time' required><br><br>
+            <p>Potwierdź lub wprowadź czas alarmowania</p>
+            <br><br>
+        <label for='alarm_time'>Czas alarmowania [s]: </label>
+        <input type='text' name='alarm_time' id='alarm_time' value='$alarm_time' required>
+        <br><br>
         <button type='submit'>Generuj wykresy</button>
     </form>";
 
-    echo $head[$lang];
-    echo $form;
-
+    showForm($head, $form);
 }
 
 function showDownloadForm(){
-    $form = "<form action='?stage=download' method='POST'>
-        <input type='checkbox' name='purge' value='purge' checked>Usuń moje pliki z serwera po ich pobraniu
+    $head = "Wyniki";
+    $form = "
+        <form action='?stage=download' method='POST'>
+            <p>Dane przetworzone. Możesz teraz pobrać wykresy oraz usunąć swoje dane z serwera</p>
+            <br><br>
+        <input type='checkbox' name='purge' value='purge' checked>Usuń moje pliki z serwera natychmiast po ich pobraniu
+        <br>
         <input type='hidden' name='download' value='download'/>
         <br>
         <button type='submit'>Pobierz wyniki</button>
         <br>
     ";
-    $pic = "<img src='uploads/".$_SESSION['name']."/visibility_cut.png' alt='visibility_cut.png'>";
+    $pic = "
+        <img class=picture src='uploads/".$_SESSION['name']."/visibility_cut.png' alt='visibility_cut.png'>
+    ";
+    $form .= $pic;
 
-    echo $form;
-    echo $pic;
+    showForm($head, $form);
 }
 
 // Generowanie unikalnego identyfikatora dla użytkownika
@@ -256,18 +280,12 @@ function echoArrays(){
     echo "<br>";
 }
 
-function showHeader(){
-    echo "<a href='index.php'><img src='home.svg' alt='Home'></a>";
-    echo "<a href='https://aamks.apoz.edu.pl/'><img src='tools.svg' alt='Tools'></a>";
-}
-
 function main(){
     if(!isset($_SESSION['name'])){
         session_start();
         setUID();
     }
 
-    showHeader();
     //echoArrays();  //for developers only
 
     if(isset($_GET['stage'])){
